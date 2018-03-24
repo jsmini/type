815542a9b9f0e0afaf1d4c9d7fd81e687581edd6
颜海镜 <yanxuefeng@meituan.com>
2018-03-24 18:01:34 +0800

    feat: update demo

--- demo/demo-node.js
+++ demo/demo-node.js
@@ -1,2 +1,9 @@
-var base = require('../dist/index.js');
-console.log(base.name);
+var type = require('../dist/index.js').type;
+
+console.log(type(123));
+
+console.log(typeof new Promise(function () {}));
+
+console.log(type(new Promise(function () {})));
+
+console.log(Object.prototype.toString.call(new Promise(function () {})));
